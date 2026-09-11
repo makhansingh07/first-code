721f5322591d9098d88f372d648c23ee4f146af1
makhansingh07 <rmakhan706@gmail.com>
2026-09-11 13:43:04 +0800

    Create if else.c

---
+++ if else.c
@@ -0,0 +1,12 @@
+/**
+ * @file hello.c
+ * @author Makhan Singh (Alwar, Rajasthan)
+ * @brief First program - Basic structure demo
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World! - My coding journey started.\n");
+    return 0;
+} 
