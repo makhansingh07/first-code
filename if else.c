6050495887e5c84a61c3e52073b1b51706517e42
makhansingh07 <rmakhan706@gmail.com>
2026-09-11 13:49:58 +0800

    Refactor hello.c to if else.c with pass/even checks

--- if else.c
+++ if else.c
@@ -1,12 +1,19 @@
-/**
- * @file hello.c
- * @author Makhan Singh (Alwar, Rajasthan)
- * @brief First program - Basic structure demo
- */
-
 #include <stdio.h>
+#define PASS_MARKS 33
+
+int isPass(int marks) { return marks >= PASS_MARKS; }
+int isEven(int num) { return num % 2 == 0; }
 
 int main() {
-    printf("Hello, World! - My coding journey started.\n");
+    int marks, num;
+    
+    printf("Marks dalo: ");
+    scanf("%d", &marks);
+    printf("Result: %s\n", isPass(marks) ? "PASS" : "FAIL");
+
+    printf("\nNumber dalo Even/Odd ke liye: ");
+    scanf("%d", &num);
+    printf("%d is %s\n", num, isEven(num) ? "EVEN" : "ODD");
+    
     return 0;
-} 
+}
